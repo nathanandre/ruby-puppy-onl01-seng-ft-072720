e1576176b52e2ad06809dae722e4676af1e987de
Nathan Burns <nathanandreburns@gmail.com>
2020-08-08 23:59:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog
+   @@all = []
+   def initialize(name)
+     @name = name
+    save
+   end
+   def save
+     @@all << self
+   end
+   def self.all
+     @@all
+   end
+  def self.print_all
+binding.pry
+    self.all.each do |pup|
+      puts pup
+    end
+  end
+   def self.clear_all
+     @@all.clear
+   end
+ end# Add your code here
